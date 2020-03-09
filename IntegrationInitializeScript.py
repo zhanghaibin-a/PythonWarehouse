c2152b40d4e4ce2fe9f4b51379327d141cd67f1f
zhanghaibin-a <1403586993@qq.com>
2020-03-10 05:20:12 +0800

    系统集成初始化linux服务器脚本，修改采用paramiko远程执行初始化

--- IntegrationInitializeScript.py
+++ IntegrationInitializeScript.py
@@ -1,140 +1,125 @@
-# coding=utf-8
-import os,time
 import paramiko
+import os
+import time
+class integration():
+    def __init__(self,ip,user='root',passwd='GKndd14@bd',system_version=7):
+        self.ip=ip
+        self.user=user
+        self.passwd=passwd
+        self.system_version=system_version
+        self.client=None
 
-def host_name(name): #修改主机名d
-	a=os.system("echo 'NETWORKING=YES' >> /etc/sysconfig/network")
-	a=os.system("echo 'HOSTNAME=%s' >> /etc/sysconfig/network" %(name))
-	a=os.system("hostnamectl set-hostname %s" %(name))
-	time.sleep(1)
-	d = os.popen("hostname")
-	print(d.read())
-
-
-def sshclient(ip,user,psswd,port="22"):  #验证网络是否正常，获取光口MAC地址 需要登陆管理机10.47.224.1
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=ip, port=port, username=user, password=psswd)
-    stdin, stdout, stderr = client.exec_command('ifconfig bond0 |grep "inet"|awk {print $1} ; cat /proc/net/bonding/bond0  |grep -E  "Slave Interface:|Permanent HW addr"')
-    print(stdout.read().decode('utf-8'))
-    client.close()
-
-def iso_yum(ip,yum_ip,user='root',psswd='GKndd14@bd',):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=ip, port=22, username=user, password=psswd)
-    stdin, stdout, stderr = client.exec_command('echo -e "[base]\nname=base\nbaseurl=ftp://%s/pub\ngpgcheck=0\nenabled=1" >/etc/yum.repos.d/base.repo'%yum_ip)
-    print(stderr.read().decode('utf-8'))
-    stdin, stdout, stderr = client.exec_command('yum clean all ; yum update -y ;yum repolist')
-    print(stdout.read().decode('utf-8'))
-    client.close()
-
-
-def sys_stop():#系统服务禁止使用6.0系统
-    sys_log=[]
-    name_stop=['NetworkManager','firewalld.service','rpcbind.socket','iptables']
-    a=os.system('type systemctl')
-    selinux_sed = os.popen("sed -i 's/enforcing\+/disabled/'  /etc/selinux/config")
-    time.sleep(2)
-    selinux_cat = os.popen("cat /etc/selinux/config |grep ^SELINUX=")
-    sys_log.append('selinux : %s' %selinux_cat.read())
-    if a==0:
-        for i in range(len(name_stop)):
-            get_back=os.system('systemctl stop %s' % (name_stop[i]))
-            if get_back ==0:
-                a=os.system('systemctl disable %s' % (name_stop[i]))
-                sys_log.append(name_stop[i]+' server stop')
-                pass
-            else:
-                sys_log.append(name_stop[i] + ' server error')
-
-    else:
-        for i in range(len(name_stop)):
-            get_back = os.system('service %d stop' % (name_stop[i]))
-            if get_back == 0:
-                a = os.system('chkconfig %d off' % (name_stop[i]))
-                sys_log.append(name_stop[i] + '\t server stop')
-            else:
-                sys_log.append(name_stop[i] + '\t server error')
-    for i in range(len(sys_log)):
-        print (sys_log[i]+'\n')
-
-
-
-def network_info(net_na_1,net_na_2,ip,gatewat="10.45.179.254"): #配置bond
-	a_name="ifcfg-"+net_na_1
-	b_name="ifcfg-"+net_na_2
-	bond_file= open("ifcfg-bond0", "w")
-	netwo="TYPE=Ethernet\nDEVICE=bond0\nBOOTPROTO=none\nONBOOT=yes\nUSERCTL=NO\nBONDING_OPTS='mode=1  miimon=100'\n" #802.3ad
-	ipadd="IPADDR="+ip+"\n"
-	netma="NETMASK=255.255.255.0"+"\n"
-	gatem="GATEWAY="+gatewat+"\n"
-	mode_a="TYPE=Ethernet\nDEVICE="+net_na_1+"\nONBOOT=yes\nBOOTPROTO=none\nMASTER=bond0\nSLAVE=yes\nUSERCTL=NO\n"
-	mode_b="TYPE=Ethernet\nDEVICE="+net_na_2+"\nONBOOT=yes\nBOOTPROTO=none\nMASTER=bond0\nSLAVE=yes\nUSERCTL=NO\n"
-	bond_info=netwo+ipadd+netma+gatem
-	bond_file.write(bond_info)
-	bond_file.close()
-	bond_a= open(a_name, "w")
-	bond_a.write(mode_a)
-	bond_a.close()
-	bond_b= open(b_name, "w")
-	bond_b.write(mode_b)
-	bond_b.close()
-	mv_l_1="mv -f  /etc/sysconfig/network-scripts/"+a_name+"   /dev/null"
-	mv_l_2="mv -f  /etc/sysconfig/network-scripts/"+b_name+"   /dev/null"
-	a=os.system(mv_l_1)
-	a=os.system(mv_l_2)
-	mv_l_3="mv ./"+a_name+" /etc/sysconfig/network-scripts"
-	mv_l_4="mv ./"+b_name+" /etc/sysconfig/network-scripts"
-	os.system(mv_l_3)
-	os.system(mv_l_4)
-	os.system("mv ./ifcfg-bond0 /etc/sysconfig/network-scripts")
-	os.system('service network restart')
-	time.sleep(10)
-	os.system('cat /proc/net/bonding/bond0')
-
-
-
-def disk_name(disk_list=[]): #磁盘挂载
-        for i in range(len(disk_list)):
-                a=["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26"]
-                c=str(a[i])
-                os.system("mkdir -p /data%s" %(c))
-                os.system("mkfs.xfs -f /dev/%s" %(disk_list[i]))
-                a=os.popen("blkid /dev/%s |awk -F: '{print $2}'|awk  '{print $1}'"%(disk_list[i]))
-                b=a.read()
-                b=b.replace("\n","")
-                disk_info="%s  /data%s    xfs     defaults 0 0"%(b,c)
-                print(disk_info)
-                os.system("echo '%s' >> /etc/fstab"%(disk_info) )
-                time.sleep(0.5)
-        os.system("mount -a ")
-
-def ssh_avoid_pass(ip,passs='GKndd14@bd',create_key=0):
-    try:
-        if len(ip)==None:
-            print("请输入IP")
+    def ssh_connect(self):
+        try:
+            """paramiko ssh初始化连接"""
+            print(self.ip,self.user,self.passwd)
+            self.client=paramiko.SSHClient()
+            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.client.connect(hostname=self.ip, port=22, username=self.user, password=self.passwd,timeout=11)
+        except BaseException as err  :
+            print(err)
             return
-        if create_key == 1:
-            os.system('rm -rf  ~/.ssh/*')
-            os.system('ssh-keygen -t rsa -P ""  -f ~/.ssh/id_rsa')
-            time.sleep(1)
-            os.system('cp -p ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys')
-        time.sleep(1)
-        print("sshpass -p %s scp -p -o StrictHostKeyChecking=no /root/.ssh/* root@%s:/root/.ssh"%(passs,ip))
-        print("sshpass -p %s ssh -o StrictHostKeyChecking=no root@%s  'mkdir /root/.ssh/' " % (passs, ip))
-        os.system("sshpass -p %s ssh -o StrictHostKeyChecking=no root@%s  'mkdir /root/.ssh/' " % (passs, ip))
-        os.system("sshpass -p %s scp -p -o StrictHostKeyChecking=no /root/.ssh/* root@%s:/root/.ssh"%(passs,ip))
-    except Exception as err:
-        print("发生错误：%s"%err)
+    def alter_host_name(self,host_name):
+        """修改系统主机名"""
+        try:
+            self.client.exec_command('echo "NETWORKING=YES" >> /etc/sysconfig/network')
+            self.client.exec_command('echo "HOSTNAME=%s" >> /etc/sysconfig/network' %(host_name))
+            if self.system_version==7:self.client.exec_command("hostnamectl set-hostname %s" %(host_name))
+            stdin, stdout, stderr =self.client.exec_command('hostname')
+        except BaseException as err  :
+            print(err)
+        else:
+            print(stdout.read().decode('utf-8'))
 
-#sys_stop()
-disk_name(["sdb","sdc","sdd","sde","sdf","sdg","sdh","sdi","sdj","sdk","sdl","sdm","sdn","sdo","sdp","sdq","sdr","sds","sdt","sdu","sdv","sdw","sdx","sdy"])
-host_name("edc-rta-cn24")
-network_info("ens4f0","ens5f1",'10.45.179.121')
-#ssh_avoid_pass(ip,'zhanghaibin0.',1)
-#for i in range(26,27):
-#    ip="10.45.179."+str(i)
-#    print(ip)
-#    ssh_avoid_pass(ip)
-#iso_yum("10.45.179.115","10.45.179.1")
+    def stop_system_service(self):
+        """停止系统服务"""
+        service_name_6=['NetworkManager','rpcbind.socket','iptables']
+        service_name_7=['NetworkManager','firewalld.service','rpcbind.socket',]
+        """停止selinux"""
+        print(self.system_version)
+        if self.system_version == 6 or self.system_version == 7:
+            self.client.exec_command('setenforce 0')
+            self.client.exec_command('sed -i "s/enforcing\+/disabled/"  /etc/selinux/config')
+            stdin, stdout, stderr =self.client.exec_command('cat /etc/selinux/config |grep ^SELINUX=')
+            print(stdout.read().decode('utf-8'))
+        else:print("停止服务没有匹配对应系统版本号")
+        if self.system_version==7:
+            for i in range(len(service_name_7)):
+                self.client.exec_command("systemctl stop %s " %(service_name_7[i]))
+                stdin, stdout, stderr=self.client.exec_command("systemctl disable %s " % (service_name_7[i]))
+                if stderr.read().decode('utf-8')=="":print("systemctl disable %s  "%service_name_7[i])
+                else:print("service no %s disable "%service_name_7[i])
+        elif self.system_version==6:
+            for i in range(len(service_name_6)):
+                self.client.exec_command("service stop %s " % (service_name_6[i]))
+                stdin, stdout, stderr=self.client.exec_command("systemctl disable %s " % (service_name_6[i]))
+                if  stderr.read().decode('utf-8')=="":print("service disable %s  "%service_name_7[i])
+                else:print("service no %s disable "%service_name_7[i])
+        else:print("停止服务没有匹配对应系统版本号")
+
+    def net_bond(self,net_na_a,net_na_b,ip,gatewat,net_type):
+        """配置bond网卡"""
+        path="/etc/sysconfig/network-scripts/"
+        #1、file bond
+        netwo = "TYPE=Ethernet\nDEVICE=bond0\nBOOTPROTO=none\nONBOOT=yes\nUSERCTL=NO\nBONDING_OPTS='mode=%s  miimon=100'\n"%net_type # backup：1---load：802.3ad
+        net_info = "IPADDR=" + ip + "\n"+"NETMASK=255.255.255.0" + "\n"+"GATEWAY=" + gatewat + "\n"
+        bond_info =netwo + net_info
+        self.client.exec_command('touch %sifcfg-bond0'%path)
+        self.client.exec_command('echo -e "%s" >%sifcfg-bond0'%(bond_info,path))
+        stdin, stdout, stderr=self.client.exec_command('cat %s%s'%(bond_info,path))
+        print(stdout.read().decode('utf-8'))
+        #2、file mode_a\mode_b
+        a_name = "ifcfg-" + net_na_a
+        b_name = "ifcfg-" + net_na_b
+        mode_a = "TYPE=Ethernet\nDEVICE=" + net_na_a + "\nONBOOT=yes\nBOOTPROTO=none\nMASTER=bond0\nSLAVE=yes\nUSERCTL=NO\n"
+        mode_b = "TYPE=Ethernet\nDEVICE=" + net_na_b + "\nONBOOT=yes\nBOOTPROTO=none\nMASTER=bond0\nSLAVE=yes\nUSERCTL=NO\n"
+        self.client.exec_command('echo -e "%s" >%s%s' % (mode_a, path,a_name))
+        self.client.exec_command('echo -e "%s" >%s%s' % (mode_b, path,b_name))
+        #3、重启网卡查看日志
+        time.sleep(5)
+        self.client.exec_command('service network restart')
+        time.sleep(10)
+        self.ssh_connect()
+        stdin, stdout, stderr=self.client.exec_command('cat /proc/net/bonding/bond0')
+        print(stdout.read().decode('utf-8'))
+
+    def disk_mount(self,mkfs_file,disk_list=[]):
+        """硬盘批量挂载"""
+        for i in range(len(disk_list)):
+            a = list(range(1, 50))
+            #1、创建文件夹
+            file_name=str(a[i])
+            self.client.exec_command("mkdir -p /data%s" %(file_name))
+            #2、格式化文件系统
+            if mkfs_file=='xfs':
+                stdin, stdout, stderr=self.client.exec_command("mkfs.%s -f /dev/%s" %(mkfs_file,disk_list[i]))
+            else:
+                stdin, stdout, stderr=self.client.exec_command("mkfs.%s -F /dev/%s" %(mkfs_file,disk_list[i]))
+            print(stderr.read().decode('utf-8'))
+            if stderr.read().decode('utf-8')=="":
+                print("格式化成功:mkfs.%s -f /dev/%s" %(mkfs_file,disk_list[i]))
+            else:
+                print("磁盘无法正常格式化,退出程序:"+stderr.read().decode('utf-8'));break
+            #3、写入/fstab
+            time.sleep(4)
+            stdin, stdout, stderr=self.client.exec_command("blkid /dev/%s |awk -F: '{print $2}'|awk  '{print $1}'"%(disk_list[i]))
+            path=stdout.read().decode("utf-8").replace("\n","")
+            print("准备挂载:"+path)
+            disk_info = "%s  /data%s    %s     defaults 0 0" % (path,file_name,mkfs_file)
+            self.client.exec_command("echo '%s' >> /etc/fstab"%(disk_info))
+        # 4、挂载配置生效,输出结果
+        self.client.exec_command("mount -a ")
+        time.sleep(2)
+        stdin, stdout, stderr = self.client.exec_command('df -hT')
+        print(stdout.read().decode('utf-8'))
+
+    def ssh_key(self):
+        """ssh免密配置"""
+        pass
+
+# ssh=integration(ip='172.16.110.128',user='root',passwd='zhanghaibin')
+# ssh.ssh_connect() #连接初始化
+# ssh.alter_host_name(host_name='txtx_email')  #主机名修改
+# ssh.stop_system_service()    #服务停止
+# ssh.net_bond(net_na_a='ens34',net_na_b='ens38',ip='192.168.1.222',gatewat='192.168.1.1',net_type='802.3ad')  #bond初始化
+# ssh.disk_mount(mkfs_file='ext4',disk_list=['sdb','sdc','sdd'])   #硬盘挂载
